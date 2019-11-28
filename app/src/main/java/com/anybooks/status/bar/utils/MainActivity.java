c6b02a89d38111c61cc48bc19bd099378561537b
臧鹏 <zangpeng@hotelgg.com>
2019-11-28 17:07:29 +0800

    test

--- app/src/main/java/com/anybooks/status/bar/utils/MainActivity.java
+++ app/src/main/java/com/anybooks/status/bar/utils/MainActivity.java
@@ -3,12 +3,14 @@ package com.anybooks.status.bar.utils;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        String name = "hello!!";
+        Log.d("zp_test", name);
     }
 }
