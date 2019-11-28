f48dae4fbd58f9c7beb4f8d789335203317904ef
臧鹏 <zangpeng@hotelgg.com>
2019-11-28 20:17:10 +0800

    add three log

--- app/src/main/java/com/anybooks/status/bar/utils/MainActivity.java
+++ app/src/main/java/com/anybooks/status/bar/utils/MainActivity.java
@@ -13,5 +13,6 @@ public class MainActivity extends AppCompatActivity {
         String name = "hello!!";
         Log.d("zp_test", name);
         Log.d("zp_test", name);
+        Log.d("zp_test", name);
     }
 }
